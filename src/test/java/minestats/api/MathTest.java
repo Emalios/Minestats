31f16b31e3d7a0f7c18ed8734175bf3c0b66637a
Hugo Bergerat <hugo.bergerat2002@gmail.com>
2026-01-10 18:58:31 +0100

    more math tests

--- src/test/java/minestats/api/MathTest.java
+++ src/test/java/minestats/api/MathTest.java
@@ -27,7 +27,7 @@ public class MathTest {
 
     private static int count = 1;
 
-    private static List<Record> genITem() {
+    private static List<Record> basicGen() {
         return List.of(
                 new Record(RecordType.ITEM, "minecraft:dirt", 10*count, CountUnit.ITEM),
                 new Record(RecordType.ITEM, "minecraft:stone", 64*count, CountUnit.ITEM),
@@ -35,7 +35,7 @@ public class MathTest {
         );
     }
 
-    private static IHandler basicItemHandler = new IHandler() {
+    private static IHandler basicHandler = new IHandler() {
         @Override
         public boolean exists() {
             return true;
@@ -43,7 +43,7 @@ public class MathTest {
 
         @Override
         public Collection<Record> getContent() {
-            return genITem();
+            return basicGen();
         }
     };
 
@@ -60,20 +60,19 @@ public class MathTest {
     @Test
     @DisplayName("Two snapshot test")
     public void twoSnapshotTest() throws InterruptedException {
-        StatPlayer player = Storage.players().getOrCreate("test-stat-math");
-        Inventory inv1 = Storage.inventories().getOrCreate("minecraft:overworld", 0, 0, 0);
+        StatPlayer player = Storage.players().getOrCreate("test-stat-math-two-snapshot");
+        Inventory inv1 = Storage.inventories().getOrCreate("test-stat-math-two-snapshot", 0, 0, 0);
         count = 1;
-        inv1.addHandler(basicItemHandler);
+        inv1.addHandler(basicHandler);
         inv1.recordContent();
         count++;
 
         TimeUnit.SECONDS.sleep(1);
         inv1.recordContent();
-        count++;
 
         player.addInventory(inv1);
         var result = StatCalculator.getInstance().genPerSecond(player.getInventories());
-        System.out.println(result);
+
         Assertions.assertFalse(result.isEmpty());
         Assertions.assertEquals(3, result.size());
         Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.ITEM, "minecraft:dirt", 10, CountUnit.ITEM), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
@@ -84,9 +83,9 @@ public class MathTest {
     @Test
     @DisplayName("ten snapshot test")
     public void tenSnapshotTest() throws InterruptedException {
-        StatPlayer player = Storage.players().getOrCreate("test-stat-math");
-        Inventory inv1 = Storage.inventories().getOrCreate("minecraft:overworld", 0, 0, 0);
-        inv1.addHandler(basicItemHandler);
+        StatPlayer player = Storage.players().getOrCreate("test-stat-math-ten-snapshot");
+        Inventory inv1 = Storage.inventories().getOrCreate("test-stat-math-ten-snapshot", 0, 0, 0);
+        inv1.addHandler(basicHandler);
         count = 1;
         //non linear values to be able to see if stats are made with these lines (5 lines)
         for (int i = 0; i < 5; i++) {
@@ -104,12 +103,92 @@ public class MathTest {
 
         player.addInventory(inv1);
         var result = StatCalculator.getInstance().genPerSecond(player.getInventories());
-        System.out.println(result);
+
         Assertions.assertFalse(result.isEmpty());
         Assertions.assertEquals(3, result.size());
         Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.ITEM, "minecraft:dirt", 10, CountUnit.ITEM), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
         Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.ITEM, "minecraft:stone", 64, CountUnit.ITEM), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
         Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.FLUID, "minecraft:water", 1000, CountUnit.MB), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
+    }
+
+    @Test
+    @DisplayName("Negative result")
+    public void negativeStat() throws InterruptedException {
+        StatPlayer player = Storage.players().getOrCreate("test-stat-math-negative");
+        Inventory inv1 = Storage.inventories().getOrCreate("test-stat-math-negative", 0, 0, 0);
+        count = -1;
+        inv1.addHandler(basicHandler);
+        inv1.recordContent();
+        count--;
+
+        TimeUnit.SECONDS.sleep(1);
+        inv1.recordContent();
+
+        player.addInventory(inv1);
+        var result = StatCalculator.getInstance().genPerSecond(player.getInventories());
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(3, result.size());
+        Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.ITEM, "minecraft:dirt", -10, CountUnit.ITEM), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
+        Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.ITEM, "minecraft:stone", -64, CountUnit.ITEM), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
+        Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.FLUID, "minecraft:water", -1000, CountUnit.MB), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
+    }
+
+    @Test
+    @DisplayName("zero result")
+    public void zeroResult() throws InterruptedException {
+        StatPlayer player = Storage.players().getOrCreate("test-stat-math-zero");
+        Inventory inv1 = Storage.inventories().getOrCreate("test-stat-math-zero", 0, 0, 0);
+        count = 1;
+        inv1.addHandler(basicHandler);
+        inv1.recordContent();
+
+        TimeUnit.SECONDS.sleep(1);
+        inv1.recordContent();
+
+        player.addInventory(inv1);
+        var result = StatCalculator.getInstance().genPerSecond(player.getInventories());
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(3, result.size());
+        Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.ITEM, "minecraft:dirt", 0, CountUnit.ITEM), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
+        Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.ITEM, "minecraft:stone", 0, CountUnit.ITEM), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
+        Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.FLUID, "minecraft:water", 0, CountUnit.MB), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
+    }
+
+    @Test
+    @DisplayName("multi inv test")
+    public void multiInvMerge() throws InterruptedException {
+        int numberOfInvs = 2;
+        StatPlayer player = Storage.players().getOrCreate("test-stat-math-ten-snapshot");
+        Inventory inv1 = Storage.inventories().getOrCreate("test-stat-math-multi-1", 0, 0, 0);
+        Inventory inv2 = Storage.inventories().getOrCreate("test-stat-math-multi-2", 0, 0, 0);
+        inv1.addHandler(basicHandler);
+        inv2.addHandler(basicHandler);
+        count = 1;
+        //non linear values to be able to see if stats are made with these lines (5 lines)
+        for (int i = 0; i < 5; i++) {
+            inv1.recordContent();
+            inv2.recordContent();
+            count*=10;
+            TimeUnit.SECONDS.sleep(1);
+        }
+        //linear values to make stats with (10 lines)
+        count = 1;
+        for (int i = 0; i < 10; i++) {
+            inv1.recordContent();
+            inv2.recordContent();
+            count++;
+            TimeUnit.SECONDS.sleep(1);
+        }
+
+        player.addInventory(inv1);
+        player.addInventory(inv2);
+        var result = StatCalculator.getInstance().genPerSecond(player.getInventories());
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(3, result.size());
+        Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.ITEM, "minecraft:dirt", numberOfInvs*10, CountUnit.ITEM), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
+        Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.ITEM, "minecraft:stone", numberOfInvs*64, CountUnit.ITEM), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
+        Assertions.assertTrue(result.contains(new Stat(new Record(RecordType.FLUID, "minecraft:water", numberOfInvs*1000, CountUnit.MB), fr.emalios.mystats.api.stat.TimeUnit.SECOND)));
     }
 
 }
